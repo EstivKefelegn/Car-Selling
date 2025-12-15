ad0fb9a6dab7d6f4259cad1bdf1300fbf20c12f7
EstivKefelegn <kefelegnestifanos0@gmail.com>
2025-12-15 21:56:24 +0300

    Creating the detail page and search

--- etiopikar/settings.py
+++ etiopikar/settings.py
@@ -58,14 +58,14 @@ CORS_ALLOW_CREDENTIALS = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '10.220.234.55:5173'
+    '192.168.1.5:5173',
     
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://10.220.234.55:5173",
+    "http://192.168.1.5:5173",
 
 ]
 
